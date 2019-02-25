3d05072028b6fcf0cc66ce2faf856e6cc41ee191
viking-v2 <31208951+viking-v2@users.noreply.github.com>
2019-02-25 14:55:44 +0100

    Most hacky solution to any problem ever

--- app/src/main/java/com/sc/marcus/calculator/MainActivity.kt
+++ app/src/main/java/com/sc/marcus/calculator/MainActivity.kt
@@ -6,6 +6,7 @@ import android.text.Editable
 import android.text.SpannableStringBuilder
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,46 +16,56 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val engine = CalculatorEngine()
-        var input = inputField.text
+        val input = StringBuilder(inputField.text.toString())
 
         btnNumber1.setOnClickListener {
-            input.append(btnNumber1.text)
+            input.append(btnNumber1.text.toString())
+            inputField.text = SpannableStringBuilder(input)
         }
 
         btnNumber2.setOnClickListener {
-            input.append(btnNumber2.text)
+            input.append(btnNumber2.text.toString())
+            inputField.text = SpannableStringBuilder(input)
         }
 
         btnNumber3.setOnClickListener {
-            input.append(btnNumber3.text)
+            input.append(btnNumber3.text.toString())
+            inputField.text = SpannableStringBuilder(input)
         }
 
         btnNumber4.setOnClickListener {
-            input.append(btnNumber4.text)
+            input.append(btnNumber4.text.toString())
+            inputField.text = SpannableStringBuilder(input)
         }
 
         btnNumber5.setOnClickListener {
-            input.append(btnNumber5.text)
+            input.append(btnNumber5.text.toString())
+            inputField.text = SpannableStringBuilder(input)
         }
 
         btnNumber6.setOnClickListener {
-            input.append(btnNumber6.text)
+            input.append(btnNumber6.text.toString())
+            inputField.text = SpannableStringBuilder(input)
         }
 
         btnNumber7.setOnClickListener {
-            input.append(btnNumber7.text)
+            input.append(btnNumber7.text.toString())
+            inputField.text = SpannableStringBuilder(input)
         }
 
         btnNumber8.setOnClickListener {
-            input.append(btnNumber8.text)
+            input.append(btnNumber8.text.toString())
+            inputField.text = SpannableStringBuilder(input)
         }
 
         btnNumber9.setOnClickListener {
-            input.append(btnNumber9.text)
+            input.append(btnNumber9.text.toString())
+            inputField.text = SpannableStringBuilder(input)
         }
 
         btnNumber0.setOnClickListener {
-            input.append(btnNumber0.text)
+            input.append(btnNumber1.text.toString())
+            inputField.text = SpannableStringBuilder(input)
         }
 
         btnAdd.setOnClickListener {
@@ -62,6 +73,7 @@ class MainActivity : AppCompatActivity() {
                Toast.makeText(this, "Only use one operator", Toast.LENGTH_SHORT).show()
            } else {
                input.append(btnAdd.text)
+               inputField.text = SpannableStringBuilder(input)
            }
         }
 
@@ -70,6 +82,7 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Only use one operator", Toast.LENGTH_SHORT).show()
             } else {
                 input.append(btnMinus.text)
+                inputField.text = SpannableStringBuilder(input)
             }
         }
 
@@ -78,6 +91,7 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Only use one operator", Toast.LENGTH_SHORT).show()
             } else {
                 input.append(btnDivide.text)
+                inputField.text = SpannableStringBuilder(input)
             }
         }
 
@@ -86,10 +100,12 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Only use one operator", Toast.LENGTH_SHORT).show()
             } else {
                 input.append(btnMultiply.text)
+                inputField.text = SpannableStringBuilder(input)
             }
         }
 
         btnClear.setOnClickListener {
+            inputField.text.clear()
             input.clear()
         }
 
@@ -97,6 +113,7 @@ class MainActivity : AppCompatActivity() {
             if(input.toString() != "" && engine.validMath(input.toString())) {
                 val edit = engine.calculateResult(input.toString())
                 inputField.text = SpannableStringBuilder(edit)
+                input.clear()
 
             } else {
                 Toast.makeText(this, "Type something valid!", Toast.LENGTH_SHORT).show()
